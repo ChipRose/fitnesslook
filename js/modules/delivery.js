05bf3de2b75a5377357f4ddd71583f63acae1179
Yana Rozova <rozova.ys@gmail.com>
2023-12-29 16:40:18 +0300

    Add payment block

--- js/modules/delivery.js
+++ js/modules/delivery.js
@@ -398,6 +398,28 @@ const setPopup = popupProperties => {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 const buttonsModal = document.querySelectorAll('.button-modal__item');
+const paymentSection = document.querySelector('.payment');
+buttonsModal.forEach(button => {
+  const modal = button.querySelector('.button-modal__modal');
+  const handleClose = () => {
+    paymentSection.classList.remove('payment--open');
+    modal.classList.remove('button-modal__modal--open');
+    window.removeEventListener('keydown', eskClose);
+  };
+  const eskClose = evt => {
+    if (evt.code === "Escape" || evt.code === "ESC") {
+      handleClose();
+    }
+  };
+  button.querySelector('.button-modal__button').addEventListener('click', () => {
+    window.addEventListener('keydown', eskClose);
+    paymentSection.classList.add('payment--open');
+    modal.classList.add('button-modal__modal--open');
+    modal.querySelector('.button-model__close-button')?.addEventListener('click', () => {
+      handleClose();
+    });
+  });
+});
 
 /***/ }),
 /* 5 */
