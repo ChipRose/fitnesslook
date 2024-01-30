d1da7ba3b8b2b2571c16edfb60b278523e7a43db
Yana Rozova <rozova.ys@gmail.com>
2024-01-30 18:30:51 +0300

    Refactoring style for tooltip

--- js/common/delivery/modal.js
+++ js/common/delivery/modal.js
@@ -1,29 +1,47 @@
 const paymentSection = document.querySelector('.payment');
 const buttonsModal = document.querySelectorAll('.button-modal__item');
 
+const eskClose = (evt) => {
+  if (evt.code === "Escape" || evt.code === "ESC") {
+    handleClose();
+  }
+}
+
 buttonsModal.forEach((button) => {
   const modal = button.querySelector('.button-modal__modal');
-  const handleClose = () => {
-    paymentSection.classList.remove('payment--open');
-    modal.classList.remove('button-modal__modal--open');
-    window.removeEventListener('keydown', eskClose);
+
+  const handleMobTooltip = () => {
+    modal.classList.add('button-modal__modal--open');
+    modal.focus();
+    modal.addEventListener('blur', () => {
+      modal.classList.remove('button-modal__modal--open');
+    })
   }
 
-  const eskClose = (evt) => {
-    if (evt.code === "Escape" || evt.code === "ESC") {
-      handleClose();
+  const handleClose = () => {
+    modal.classList.remove('button-modal__modal--open');
+  }
+
+  const handleDeskTooltip = () => {
+    modal.classList.add('button-modal__modal--open');
+    button.addEventListener('mouseout', handleClose, { once: true });
+  }
+
+  const handleWindowResize = () => {
+    const windowWidth = window.innerWidth;
+    const actElement = button.querySelector('.button-modal__button');
+
+    if (windowWidth < 768) {
+      actElement?.removeEventListener('mouseout', handleClose);
+      actElement?.removeEventListener('mouseover', handleDeskTooltip);
+      actElement?.addEventListener('click', handleMobTooltip);
+    } else {
+      actElement?.removeEventListener('click', handleMobTooltip);
+      actElement?.addEventListener('mouseover', handleDeskTooltip);
     }
   }
 
-  button.querySelector('.button-modal__button')?.addEventListener('click', () => {
-    window.addEventListener('keydown', eskClose);
-
-    paymentSection.classList.add('payment--open');
-    modal.classList.add('button-modal__modal--open');
-
-    modal.querySelector('.button-model__close-button')?.addEventListener('click', () => {
-      handleClose();
-    })
-  });
+  handleWindowResize(button, modal);
+  window.addEventListener('resize', handleWindowResize);
 })
 
