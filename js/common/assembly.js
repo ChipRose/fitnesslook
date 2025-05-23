499690a5f50c498c2e04b01755ddbd8aeccdeffe
ChipRose <yana.rozova@mail.ru>
2025-05-23 14:03:37 +0000

    Deploying to gh-page from @ ChipRose/fitnesslook@cb86f8b86d96a1a72f0d4db0fcc7e9ae2cf5414a 🚀

--- js/common/assembly.js
+++
@@ -1,7 +0,0 @@
-import { setAccordions } from './util/accordion.js';
-import { setNavigation } from './util/navigation.js';
-
-setNavigation('#navigation-in');
-setNavigation('#global-up');
-
-setAccordions('.accordion-simple');
